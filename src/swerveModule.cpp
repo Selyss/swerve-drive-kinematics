ef9c32fa145c1c5eae6e9fccc8723c54e7158d3c
Selyss <99344963+Selyss@users.noreply.github.com>
2025-07-06 19:24:55 -0400

    work on the swerve module

--- src/swerveModule.cpp
+++ src/swerveModule.cpp
@@ -2,6 +2,16 @@
 
 SwerveModule::SwerveModule() : cTheta(0.0), cVelocity(0.0), tTheta(0.0), tSpeed(0.0) {}
 
+float SwerveModule::getDriveOutput() const
+{
+    return tSpeed;
+}
+
+float SwerveModule::getSteerOutput() const
+{
+    return tTheta;
+}
+
 void SwerveModule::setTarget(float angle, float speed)
 {
     // validate later lol
@@ -20,6 +30,7 @@ float SwerveModule::normalizeAngle(float angle)
     return normalizedAngle;
 }
 
+// Calculates the shortest signed angle to the target
 float SwerveModule::shortestAngleDiff(float a, float b)
 {
     return std::abs(a - b) < std::abs(b - a) ? a - b : b - a;
@@ -32,4 +43,8 @@ void SwerveModule::optimizeTarget()
         tSpeed = -tSpeed;
         tTheta = -tTheta;
     }
+}
+
+void SwerveModule::update()
+{
 }
--- src/swerveModule.h
+++ src/swerveModule.h
@@ -16,6 +16,6 @@ public:
 
     void setTarget(float angle, float speed);
     void update();
-    float getDriveOutput() const;
-    float getSteerOutput() const;
+    float getDriveOutput();
+    float getSteerOutput();
 };
