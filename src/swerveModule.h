c73a566c05c9b8cdb6c69118a8b22afd7dae6655
alonso130r <131227801+alonso130r@users.noreply.github.com>
2025-07-06 18:27:01 -0400

    Add SwerveModule class with basic structure and methods

---
+++ src/swerveModule.h
@@ -0,0 +1,19 @@
+#include <cmath>
+
+class SwerveModule {
+private:
+    float cTheta, cVelocity;
+    float tTheta, tVelocity;
+
+    float normalizeAngle(float angle);
+    void optimizeTarget();
+    float shortestAngleDiff(float a, float b);
+
+public:
+    SwerveModule();
+
+    void setTarget(float angle, float speed);
+    void update();
+    float getDriveOutput() const;
+    float getSteerOutput() const;
+};
