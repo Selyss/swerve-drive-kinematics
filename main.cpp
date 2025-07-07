bcb0196700d2297ef0ec98a57cba0ac3c298e736
Selyss <99344963+Selyss@users.noreply.github.com>
2025-07-07 23:24:19 +0000

    seems to work

--- main.cpp
+++ main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include "robot.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc != 4)
+    {
+        std::cerr << "Usage: " << argv[0] << " <tx> <ty> <tOmega>" << std::endl;
+        return 1;
+    }
+
+    float tx = std::stof(argv[1]);
+    float ty = std::stof(argv[2]);
+    float tOmega = std::stof(argv[3]);
+
     Robot robot(1.0f); // 1m square for testing
 
-    // test driving forward with no rotation
-    robot.drive(1.0f, 0.0f, 0.0f);
+    robot.drive(tx, ty, tOmega);
     robot.update();
 
-    std::cout << "Robot position after driving forward 1m: "
+    std::cout << "Robot position after applying input: "
               << "X: " << robot.getX() << ", "
               << "Y: " << robot.getY() << ", "
               << "Theta: " << robot.getTheta() << std::endl;
