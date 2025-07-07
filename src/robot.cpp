94a9e99b06b220a9bf6a5e661d83cc2805c851ec
alonso130r <131227801+alonso130r@users.noreply.github.com>
2025-07-06 22:27:43 -0400

    style: improve code formatting and consistency in Robot class methods

--- src/robot.cpp
+++ src/robot.cpp
@@ -8,26 +8,28 @@ Robot::Robot(float dim) : dim(dim)
     brPos = std::make_pair(-dim / 2, -dim / 2);
 }
 
-void Robot::drive(float tvx, float tvy, float tOmega) {
+void Robot::drive(float tvx, float tvy, float tOmega)
+{
     float tlSpeed, trSpeed, blSpeed, brSpeed;
     float tlTheta, trTheta, blTheta, brTheta;
     float maxSpeed;
 
-    tlSpeed = std::sqrt((vx - (omega * tlPos.second))*(vx - (omega * tlPos.second)) + (vy + (omega * tlPos.first))*(vy + (omega * tlPos.first)));
+    tlSpeed = std::sqrt((vx - (omega * tlPos.second)) * (vx - (omega * tlPos.second)) + (vy + (omega * tlPos.first)) * (vy + (omega * tlPos.first)));
     tlTheta = std::atan2f((vy + (omega * tlPos.first)), (vx - (omega * tlPos.second)));
 
-    trSpeed = std::sqrt((vx - (omega * trPos.second))*(vx - (omega * trPos.second)) + (vy + (omega * trPos.first))*(vy + (omega * trPos.first)));
+    trSpeed = std::sqrt((vx - (omega * trPos.second)) * (vx - (omega * trPos.second)) + (vy + (omega * trPos.first)) * (vy + (omega * trPos.first)));
     trTheta = std::atan2f((vy + (omega * trPos.first)), (vx - (omega * trPos.second)));
 
-    blSpeed = std::sqrt((vx - (omega * blPos.second))*(vx - (omega * blPos.second)) + (vy + (omega * blPos.first))*(vy + (omega * blPos.first)));
+    blSpeed = std::sqrt((vx - (omega * blPos.second)) * (vx - (omega * blPos.second)) + (vy + (omega * blPos.first)) * (vy + (omega * blPos.first)));
     blTheta = std::atan2f((vy + (omega * blPos.first)), (vx - (omega * blPos.second)));
 
-    brSpeed = std::sqrt((vx - (omega * brPos.second))*(vx - (omega * brPos.second)) + (vy + (omega * brPos.first))*(vy + (omega * brPos.first)));
+    brSpeed = std::sqrt((vx - (omega * brPos.second)) * (vx - (omega * brPos.second)) + (vy + (omega * brPos.first)) * (vy + (omega * brPos.first)));
     brTheta = std::atan2f((vy + (omega * brPos.first)), (vx - (omega * brPos.second)));
 
     maxSpeed = std::max(std::max(tlSpeed, trSpeed), std::max(blSpeed, brSpeed));
 
-    if (maxSpeed > 1.0) {
+    if (maxSpeed > 1.0)
+    {
         tlSpeed = tlSpeed / maxSpeed;
         trSpeed = trSpeed / maxSpeed;
         blSpeed = blSpeed / maxSpeed;
@@ -40,7 +42,8 @@ void Robot::drive(float tvx, float tvy, float tOmega) {
     brModule.setTarget(brTheta, tlSpeed);
 }
 
-void Robot::update() {
+void Robot::update()
+{
     tlModule.update();
     trModule.update();
     blModule.update();
